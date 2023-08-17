c4f48368af20019f305264367d6b768cea220b22
fillipi.mangrich <costa.mangrich@gmail.com>
2023-08-17 14:14:27 -0300

    remove image and add base layout

--- src/View/View.py
+++ src/View/View.py
@@ -6,19 +6,30 @@ class View():
 
     def run(self) -> None:
         self.__window.geometry("1280x720")
-        self.__window.configure(bg = "#000000")
-        self.setCanvas()
-        self.setBackground()
+        self.__window.configure(bg = "#2D2D2D")
+        self.setViewPort()
+        self.setLogOfActionsView()
+        self.setListOfObjectsView()
         self.__window.mainloop()
 
-    def setCanvas(self) -> None:
-        self.__canvas = tk.Canvas(
-            self.__window,bg = "#000000",height = 720,width = 1280,bd = 0,highlightthickness = 0,relief = "ridge")
-        self.__canvas.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+    def setViewPort(self) -> None:
+        self.__view_port = tk.Canvas(
+            self.__window,bg = "#FFFFFF",height = 460,width = 920,bd = 0,highlightthickness = 0,relief = "ridge")
+        self.__view_port.place(x=340, y=20)
 
-    def setBackground(self) -> None:
-        self.__background_img = tk.PhotoImage(file = f"src\Images\Base.png")
-        background = self.__canvas.create_image(0, 0,image=self.__background_img,anchor="nw")
+    def setLogOfActionsView(self) -> None:
+        self.__log_actions_view = tk.Canvas(
+            self.__window,bg = "#FFFFFF",height = 200,width = 920,bd = 0,highlightthickness = 0,relief = "ridge")
+        self.__log_actions_view.place(x=340, y=500)
+    
+    def setListOfObjectsView(self) -> None:
+        self.__list_of_objects_view = tk.Canvas(
+            self.__window,bg = "#FFFFFF",height = 240,width = 300,bd = 0,highlightthickness = 0,relief = "ridge")
+        self.__list_of_objects_view.place(x=20, y=20)
+
+
+
+
 
 app = View()
 app.run()
