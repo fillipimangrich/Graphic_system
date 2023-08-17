bfa019e63ee9677862a2ecfd989f4b0c8fdcc950
fillipi.mangrich <costa.mangrich@gmail.com>
2023-08-17 01:47:00 -0300

    add background image (need to fix resolution)

--- src/View/View.py
+++ src/View/View.py
@@ -4,16 +4,21 @@ class View():
     def __init__(self) -> None:
         self.__window = tk.Tk()
 
-    def run(self):
-        self.setCanvas()
+    def run(self) -> None:
         self.__window.geometry("1280x720")
         self.__window.configure(bg = "#000000")
+        self.setCanvas()
+        self.setBackground()
         self.__window.mainloop()
 
     def setCanvas(self) -> None:
         self.__canvas = tk.Canvas(
             self.__window,bg = "#000000",height = 720,width = 1280,bd = 0,highlightthickness = 0,relief = "ridge")
         self.__canvas.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+
+    def setBackground(self) -> None:
+        self.__background_img = tk.PhotoImage(file = f"src\Images\Base.png")
+        background = self.__canvas.create_image(0, 0,image=self.__background_img,anchor="nw")
 
 app = View()
 app.run()
