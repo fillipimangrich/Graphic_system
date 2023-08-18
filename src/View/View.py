b0bf2e31e2662318a7d0dc551a0b179a9e22c2ad
fillipi.mangrich <costa.mangrich@gmail.com>
2023-08-18 16:07:22 -0300

    new buttons

--- src/View/View.py
+++ src/View/View.py
@@ -21,6 +21,7 @@ class View():
         self.__view_port.place(x=340, y=20)
         self.__view_port.bind("<Button-1>", self.draw)
 
+
     def setLogOfActionsView(self) -> None:
         self.__log_actions_view = tk.Canvas(
             self.__window,bg = "#FFFFFF",height = 200,width = 920,bd = 0,highlightthickness = 0,relief = "ridge")
@@ -36,11 +37,26 @@ class View():
             self.__window,bg = "#565656",height = 420,width = 300,bd = 0,highlightthickness = 0,relief = "ridge")
         self.__control.place(x=20, y=280)
         self.setAddObjectButton()
+        self.setZoomButtons()
+        self.setMoveButtons()
 
     def setAddObjectButton(self) -> None:
         self.__add_object_button = tk.PhotoImage(file = f"src/Images/add_button.png")
         button_add = self.__control.create_image(220, 50, image=self.__add_object_button)
-        # self.__control.tag_bind(button_add, "<Button-1>", lambda x: )
+        self.__control.tag_bind(button_add, "<Button-1>", lambda x: print("Adicionou objeto"))
+
+
+    def setZoomButtons(self) -> None:
+        self.__zoom_in_button = tk.PhotoImage(file=f"src\Images\zoomIn.png")
+        button_zoom_in = self.__control.create_image(20, 25, image = self.__zoom_in_button)
+        self.__control.tag_bind(button_zoom_in, "<Button-1>", lambda x: print("Zoom In") )
+        self.__zoon_out_button = tk.PhotoImage(file =f"src\Images\zoomOut.png")
+        button_zoom_out = self.__control.create_image(20, 70, image = self.__zoon_out_button)
+        self.__control.tag_bind(button_zoom_out, "<Button-1>", lambda x: print("Zoom Out") )
+
+    def setMoveButtons(self) -> None:
+        pass
+
 
     def openViewForAddNewShape(self) -> None:
         pass
@@ -62,3 +78,7 @@ class View():
 
     def update(self):
         pass
+
+    def test(self):
+        button1 = tk.Button(text='Get the Square Root', bg='brown', fg='white', font=('helvetica', 9, 'bold'))
+        self.__view_port.create_window(200, 180, window=button1)
