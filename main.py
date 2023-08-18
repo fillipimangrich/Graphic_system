8f3fe9e0bc77fad696ee7789465adb15c846155b
fillipi.mangrich <costa.mangrich@gmail.com>
2023-08-17 21:13:53 -0300

    add control canvas and add button

---
+++ main.py
@@ -0,0 +1,5 @@
+from src.View.View import View
+
+if (__name__ == "__main__"):
+    app = View()
+    app.run()
--- src/View/View.py
+++ src/View/View.py
@@ -10,6 +10,8 @@ class View():
         self.setViewPort()
         self.setLogOfActionsView()
         self.setListOfObjectsView()
+        self.setControlView()
+        self.setAddObjectButton()
         self.__window.mainloop()
 
     def setViewPort(self) -> None:
@@ -27,9 +29,13 @@ class View():
             self.__window,bg = "#FFFFFF",height = 240,width = 300,bd = 0,highlightthickness = 0,relief = "ridge")
         self.__list_of_objects_view.place(x=20, y=20)
 
+    def setControlView(self) -> None:
+        self.__control = tk.Canvas(
+            self.__window,bg = "#565656",height = 420,width = 300,bd = 0,highlightthickness = 0,relief = "ridge")
+        self.__control.place(x=20, y=280)
+        self.setAddObjectButton()
 
-
-
-
-app = View()
-app.run()
+    def setAddObjectButton(self) -> None:
+        self.__add_object_button = tk.PhotoImage(file = f"src/Images/add_button.png")
+        button_add = self.__control.create_image(220, 50, image=self.__add_object_button)
+        # self.__control.tag_bind(button_add, "<Button-1>", lambda x: )
